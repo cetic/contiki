cetic/6lbr

Clean up mix between RTR_SOLICITATION_INTERVAL, MAX_RTR_SOLICITATION_INTERVAL and MAX_RTR_SOLICITATION_DELAY

core/net/ipv6/uip-ds6.c
@@ -166,13 +166,19 @@ uip_ds6_init(void)
 #endif /* UIP_ND6_SEND_RA */
 #if UIP_CONF_6LR
   etimer_set(&uip_ds6_timer_rs,
-             random_rand() % (UIP_ND6_MAX_RTR_SOLICITATION_DELAY *
+             random_rand() % (UIP_ND6_RTR_SOLICITATION_INTERVAL *
                               CLOCK_SECOND));
 #endif /* UIP_CONF_6LR */
 #else /* UIP_CONF_ROUTER */
+#if CONF_6LOWPAN_ND
+  etimer_set(&uip_ds6_timer_rs,
+             random_rand() % (UIP_ND6_RTR_SOLICITATION_INTERVAL *
+                              CLOCK_SECOND));
+#else
   etimer_set(&uip_ds6_timer_rs,
              random_rand() % (UIP_ND6_MAX_RTR_SOLICITATION_DELAY *
                               CLOCK_SECOND));
+#endif /* CONF_6LOWPAN_ND */
 #endif /* UIP_CONF_ROUTER */
   etimer_set(&uip_ds6_timer_periodic, UIP_DS6_PERIOD);
 #if CONF_6LOWPAN_ND_OPTI_START && UIP_CONF_6L_ROUTER

cetic/contiki

core/net/ipv6/uip-ds6.c
@@ -158,13 +158,19 @@ uip_ds6_init(void)
 #endif /* UIP_ND6_SEND_RA */
 #if UIP_CONF_6LR
   etimer_set(&uip_ds6_timer_rs,
-             random_rand() % (UIP_ND6_MAX_RTR_SOLICITATION_DELAY *
+             random_rand() % (UIP_ND6_MAX_RTR_SOLICITATION_INTERVAL *
                               CLOCK_SECOND));
 #endif /* UIP_CONF_6LR */
 #else /* UIP_CONF_ROUTER */
+#if CONF_6LOWPAN_ND
+  etimer_set(&uip_ds6_timer_rs,
+             random_rand() % (UIP_ND6_MAX_RTR_SOLICITATION_INTERVAL *
+                              CLOCK_SECOND));
+#else
   etimer_set(&uip_ds6_timer_rs,
              random_rand() % (UIP_ND6_MAX_RTR_SOLICITATION_DELAY *
                               CLOCK_SECOND));
+#endif /* CONF_6LOWPAN_ND */
 #endif /* UIP_CONF_ROUTER */
   etimer_set(&uip_ds6_timer_periodic, UIP_DS6_PERIOD);
 #if CONF_6LOWPAN_ND_OPTI_START && UIP_CONF_6L_ROUTER